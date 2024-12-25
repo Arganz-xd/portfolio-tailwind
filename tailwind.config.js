55b474cf67a21f84b9d110fd827d72494d4b9057
Arganz-xd <arganztea.ans@gmail.com>
2024-12-25 21:02:07 +0700

    update dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -9,8 +10,8 @@ module.exports = {
     extend: {
       colors: {
         primary: "#D8D2C2",
-        secondary: '#0f172a',
-        dark: "#4A4947",
+        secondary: '#64748b',
+        dark: "#1A1A1D",
       },
       screens: {
         '2xl': "1320px",
